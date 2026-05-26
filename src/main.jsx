e31ee7e6bd78bcafb07237a5e8a3f90a92d0ab5e
Yaiyneabeba <139072009+Cep0110@users.noreply.github.com>
2026-05-27 02:53:45 +0300

    Import index.css in main.jsx
    Added import for index.css to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Strictly capitalized to match file name
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
