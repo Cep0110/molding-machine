5b351d866972aa8e95ca86ce797ad0a0e4194115
Yaiyneabeba <139072009+Cep0110@users.noreply.github.com>
2026-05-27 03:01:32 +0300

    Comment out CSS import in main.jsx
    Commented out the CSS import to prevent Rollup from crashing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Strictly capitalized to match file name
-import './index.css'
+import App from './App.jsx' 
+
+// Deleted or commented out to prevent Rollup from crashing
+// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
